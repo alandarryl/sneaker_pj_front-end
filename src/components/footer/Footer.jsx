4ea3fe75a7de6a3508cfd98fd4e37bb5181442b7
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-12-10 12:56:47 +0100

    add structure to component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,23 @@
+
+
+import "./footer.css";
+
+
+function Footer() {
+
+    return(
+        <div className="footer" >
+            <p className="footer-text" >© 2024 Sneaker Store. All rights reserved.</p>
+            <h2 className="footer-title" >sneaker</h2>
+            <ul className="footer-links" >
+                <li className="footer-link-item" >facebook</li>
+                <li className="footer-link-item" >instagram</li>
+                <li className="footer-link-item" >tic-toc</li>
+                <li className="footer-link-item" >twitter</li>
+            </ul>
+        </div>
+    )
+
+}
+
+
