7eb72088d294a4f888219fb48510fa36c1f69856
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-12-22 13:54:27 +0100

    add the need page on the file tree

---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,13 @@
+
+
+
+function DetailPage() {  return (
+    <div>
+        <h1>Detail Page</h1>
+        <p>This is the detail page content.</p>
+    </div>
+    );
+}
+
+
+export default DetailPage;
---
+++ src/pages/FilterPage.jsx
@@ -0,0 +1,14 @@
+
+
+function FilterPage() {
+    return (
+    <div>
+        <h1>Filter Page</h1>
+      {/* Filter functionality will be implemented here */}
+    </div>
+    );
+}
+
+export default FilterPage;
+
+
