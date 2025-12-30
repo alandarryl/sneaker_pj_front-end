ce7135ad61823ccec37be604d270e04bc8eff563
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-12-30 21:06:26 +0100

    add content to the component filter grid

--- src/components/filterList/FilterGrid.jsx
+++ src/components/filterList/FilterGrid.jsx
@@ -1,12 +1,34 @@
 
+import "./filterGrid.css";
+
+import Card from "../card/Card";
+
+import useFetch from "../../hooks/useFetch";
 
 import './filterGrid.css';
 
 
 function FilterGrid(){
 
+    const {
+    data: productList,
+    loading,
+    error,
+    } = useFetch("https://sneaker-pj-back-end.vercel.app/api/sneakers/all");
+
     return(
-        <div></div>
+        <div className="filter-grid" >
+            {productList.map((product) =>(
+                <Card
+                    className="card"
+                    key={product.id}
+                    id={product.id}
+                    title={product.name}
+                    img={product.picture?.[0]?.pic1}
+                    price={product.price}
+                />
+            ) )}
+        </div>
     )
 
 }
