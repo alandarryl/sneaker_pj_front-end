81e97205630478c19a8897c111cbc77b14a19e94
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-12-31 08:27:11 +0100

    add style to filter grid and element to filter setting

--- src/components/filterSetting/FilterSetting.jsx
+++ src/components/filterSetting/FilterSetting.jsx
@@ -7,7 +7,17 @@ import './filterSetting.css';
 function FilterSetting(){
 
     return(
-        <div></div>
+        <div className='filter-setting' >
+            <div className='filter-search-bar' >
+                <input/>
+                <button>Search</button>
+            </div>
+            <select className='filter-select' >
+                <option value={none} >first</option>
+                <option value={h} >Male</option>
+                <option value={f} >Female</option>
+            </select>
+        </div>
     )
 
 }
